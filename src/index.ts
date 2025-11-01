95229e8b88dccf86a18809e0ed2a86e0d9c90144
Glavin Wiechert <glavin.wiechert@gmail.com>
2025-11-01 00:10:33 -0300

    fix package.json exports and update index.ts exports for improved module structure

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,27 @@ import Planner from "./planner";
 import DomainBuilder from "./domainBuilder";
 import Effect from "./effect";
 import EffectType from "./effectType";
+import DecompositionStatus from "./decompositionStatus";
 import FuncCondition from "./conditions/funcCondition";
 import FuncOperator from "./operators/funcOperator";
+import PrimitiveTask from "./Tasks/primitiveTask";
 import TaskStatus from "./taskStatus";
 
-export { Domain, Context, Planner, DomainBuilder, Effect, FuncCondition, FuncOperator, TaskStatus };
+export {
+  Domain,
+  Context,
+  Planner,
+  DomainBuilder,
+  Effect,
+  EffectType,
+  DecompositionStatus,
+  FuncCondition,
+  FuncOperator,
+  PrimitiveTask,
+  TaskStatus,
+};
+
+export type { EffectTypeValue } from "./effectType";
 
 export default {
   Domain,
@@ -17,7 +33,9 @@ export default {
   DomainBuilder,
   Effect,
   EffectType,
+  DecompositionStatus,
   FuncCondition,
   FuncOperator,
+  PrimitiveTask,
   TaskStatus,
 };
